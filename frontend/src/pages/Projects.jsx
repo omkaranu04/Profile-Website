8a409cfba2494408721885d36083e2321a42b0d5
Omkar <omkaranu04@gmail.com>
2025-12-13 18:32:27 +0530

    added projects

--- frontend/src/pages/Projects.jsx
+++ frontend/src/pages/Projects.jsx
@@ -12,6 +12,88 @@ const projects = [
     • Conducted extensive ablation studies isolating the contributions of CBAM, FZLPR, and training-schedule components \n
     • Assessed robustness under domain shift using an unseen Indian dataset; targeted fine-tuning lifted F1-Score from 74.84% to 96.70% \n
     `
+  },
+  {
+    id: 'proj-b',
+    title: 'VAE and GAN - Advanced Machine Learning Course',
+    timeframe: 'Oct \'25',
+    github: 'https://github.com/omkaranu04/Advanced-ML_CS60073-Autumn-25/tree/master/Assignments/Assignment%203',
+    description:`
+    • Implemented a VAE with a 16-dimensional latent space, trained via ELBO, generating reconstruction and generation curves\n
+    • Trained an MLP-based GAN with LeakyReLU, dropout-regularized discriminator, Tanh generator, and label smoothing\n
+    • Compared MNIST-trained VAE and GAN using Inception Score and Fréchet Distance, computed with a custom CNN classifier\n
+    `
+  },
+  {
+    id: 'proj-c',
+    title:'Mini Shell - Self Project',
+    timeframe: 'May \`25',
+    github: 'https://github.com/omkaranu04/Mini-Shell',
+    description:`
+    • Built a robust Unix Shell in C supporting custom built-in commands, PATH-based search, and external executable handling\n
+    • Implemented pipes, I/O-redirections, globbing, and heredoc streaming via fork/execve/dup2/pipe for POSIX compliance\n
+    • Managed signals and terminal nodes for Bash-like Ctrl-C/Ctrl-\handling, signal-safe cleanup, and consistent state restoration\n
+    • Ensured leak-free parsing of quotes, environment variable expansion, and operators with syntax validation and memory checks\n
+    `
+  },
+  {
+    id: 'proj-d',
+    title: 'Graph Database System - Database Management Systems Laboratory',
+    timeframe: 'Mar \'25 - Apr \'25',
+    github: 'https://github.com/omkaranu04/DBMS-Term-Project',
+    description:`
+    • Designed a Neo4j recommendation system on Amazon SNAP dataset (500K+ nodes, 1M+ edges), using product-user relations\n
+    • Built a RAG ChatBot for search, indexing 100K+ embeddings with FAISS for sub-100ms retrieval and BAAI for semantics\n
+    • Developed a two-phase PageRank algorithm with 5-iteration convergence, blending helpfulness and co-purchase influence\n
+    • Integrated route-level performance logging via a custom decorator logging query latency, CPU, and memory usage per API call\n
+    `
+  },
+  {
+    id: 'proj-e',
+    title: 'Reliable Transport Protocol for Network Communication - Computer Networks Laboratory',
+    timeframe: 'Mar \`25',
+    github: 'https://github.com/omkaranu04/Networks_Lab-CS39006-Spring-25/tree/master/Assignment%204%20Final',
+    description: `
+      • Designed a reliability layer atop UDP, with packet framing, flow/error control, and retransmission in lossy environments\n
+      • Architected a multi-threaded protocol daemon with modular sender, receiver, and GC threads, for maximizing throughput\n
+      • Utilized shared memory and mutex locks to synchronize access for 5 sockets, enabling concurrent operations across processes\n
+      • Implemented selective-repeat sliding window with sequence-numbered DATA/ACK packets for ordered delivery and fast recovery\n
+    `
+  },
+  {
+    id: 'proj-f',
+    title: 'Automatic Image Captioning - Deep Learning Course',
+    timeframe: 'Mar \'25 - Apr \'25',
+    github: 'https://github.com/omkaranu04/DL-CS60010-Spring-25/tree/master/Assignments/Assignment%202',
+    description:`
+    • Developed a ViT-GPT2 encoder-decoder for image captioning, achieving BERTScore F1 (0.536) vs SmolVLM (0.515) on the test set\n
+    • Demonstrated robustness under occlusion - model kept the BERTScore F1 of 0.534 at 80% masking, outperforming SmolVLM's 0.474\n
+    • Benchmarked semantic alignment and stability - custom model showed lesser drop than SmolVLM under increasing occlusion\n
+    `
+  },
+  {
+    id: 'proj-g',
+    title: 'KGP-miniRISC Processor - Computer Organization and Architecture Laboratory',
+    timeframe: 'Oct \'24 - Nov \'24',
+    github: 'https://github.com/omkaranu04/KGP-miniRISC-Processor',
+    description:`
+    • Designed a custom hardwired control unit with tailored instruction encoding, enabling efficient decoding and streamlined control flow\n
+    • Implemented a BRAM-integrated memory with ALU, register-memory and branching instructions, ensuring efficient execution\n
+    • Synthesized and deployed processor architecture on FPGA, conducting functional verification with test benches and instruction sets\n
+    • Defined a 40-instruction RISC ISA and control-signal map, leveraging Harvard architecture for separate instruction and data paths\n
+    `
+  },
+  {
+    id: 'proj-h',
+    title: 'Work Management System - Software Engineering Laboratory',
+    timeframe: 'Mar \'24 - Apr \'24',
+    github: 'https://github.com/omkaranu04/Work-Management-System',
+    description:`
+    • Developed a multi-user platform with role-based access, supporting manual and automated assignment and real-time progress tracking\n
+    • Built dynamic interfaces for adding, editing, searching, and priority assignment of works and workers, enabling efficient management\n
+    • Engineered a responsive frontend (HTML, CSS, JS) and Node.js backend with MongoDB, with bcrypt authentication, for fast operations\n
+    • Adopted Agile SDLC with daily reviews, executing unit, system, and regression testing, passing across all user and assignment flows\n
+    `
   }
 ]
 
